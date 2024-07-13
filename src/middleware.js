c1a2acf7d4760fc9e35a6951d164da063b73c4b7
hyungjin.lee <hyungjin.lee@emotion.co.kr>
2024-07-14 00:50:17 +0900

    COMMIT

--- src/middleware.js
+++ src/middleware.js
@@ -4,11 +4,14 @@ import { NextResponse } from "next/server";
 export async function middleware(req) {
   const token = await getToken({ req });
   const url = req.nextUrl.clone();
+  console.log("token", token);
+  console.log("url", url);
 
   if (!token) {
     // TODO : 토큰이 업을때 로그인페이지로 보내고 (login폴더로 페이지 하나 따야함)
-    // url.pathname = "/";
+    url.pathname = "/login";
     // 토큰이 있을때 그냥 보내기
+    NextResponse.redirect(url);
     // return NextResponse.redirect(url);
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 
 import Head from "next/head";
-import { Modal } from "@/pages/components/modal";
+// import { Modal } from "@/pages/components/modal";
 
 const Home = () => {
-  const [on, setOn] = useState(false);
+  // const [on, setOn] = useState(false);
   return (
     <>
       <Head>
         <title>간편로그인</title>
       </Head>
       <main>
-        <button onClick={() => setOn(true)}>LOGIN</button>
-        <Modal on={on} setOn={setOn} />
+        {/*<button onClick={() => setOn(true)}>LOGIN</button>*/}
+        {/*<Modal on={on} setOn={setOn} />*/}
       </main>
     </>
   );
