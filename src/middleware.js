b288963f9c685e086fa298a96275bb7722b1faeb
hyungjin.lee <hyungjin.lee@emotion.co.kr>
2024-07-15 18:39:29 +0900

    COMMIT

--- src/middleware.js
+++ src/middleware.js
@@ -6,10 +6,10 @@ export async function middleware(req) {
   const url = req.nextUrl.clone();
   // console.log(token);
 
-  if (!token) {
-    // TODO : 토큰이 업을때 로그인페이지로 보내고 (login폴더로 페이지 하나 따야함)
-    // url.pathname = "/login";
-    // 토큰이 있을때 그냥 보내기
-    // return NextResponse.redirect(url);
-  }
+  // if (!token) {
+  //   // TODO : 토큰이 업을때 로그인페이지로 보내고 (login폴더로 페이지 하나 따야함)
+  //   // url.pathname = "/login";
+  //   // 토큰이 있을때 그냥 보내기
+  //   // return NextResponse.redirect(url);
+  // }
 }
