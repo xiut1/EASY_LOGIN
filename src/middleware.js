7ea7d59a270525a6d2920fb7cf4d995834e81da2
hyungjin.lee <hyungjin.lee@emotion.co.kr>
2024-07-15 18:32:30 +0900

    COMMIT

--- src/middleware.js
+++ src/middleware.js
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function middleware(req) {
   const token = await getToken({ req });
   const url = req.nextUrl.clone();
-  console.log(token);
+  // console.log(token);
 
   if (!token) {
     // TODO : 토큰이 업을때 로그인페이지로 보내고 (login폴더로 페이지 하나 따야함)
