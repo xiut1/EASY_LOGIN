de82975fbb10ee4050c12e019a670523215ef5a9
hyungjin.lee <hyungjin.lee@emotion.co.kr>
2024-07-16 15:37:32 +0900

    COMMIT

--- src/middleware.js
+++ src/middleware.js
@@ -4,11 +4,11 @@ import { NextResponse } from "next/server";
 export async function middleware(req) {
   // 요청에 포함된 URL을 복사하여 'nextUrl' 변수에 저장한다
   const nextUrl = req.nextUrl.clone();
-
   const token = await getToken({ req });
 
-  // 토큰이 없으면, 'nextUrl.pathname'을 "/login"으로 보내버린다.
+  // 토큰이 없으면, "/login"으로 보낸다.
   if (!token) {
+    alert('로그인 필요')
     nextUrl.pathname = "/login";
     return NextResponse.redirect(nextUrl);
     // NextResponse.redirect('/login')로 사용하면 될거같지만 "/" 부분이 "%2F"로 인코딩되어 에러를 발생시킴.
